2d38c1115ef03097671db71c7c3f20a7443c0842
Evan Briones <evrick@email.arizona.edu>
2013-03-04 10:03:25 -0700

    Improved gene_order script.
    * Added more error checking.
    * Fixed sorting not being assigned.
    * Added changing log level to script.
    * Other minor fixes.

--- tools/synmap/gene_order.py
+++ tools/synmap/gene_order.py
@@ -8,14 +8,14 @@ import os
 import re
 import sys
 
-logger = logging.getLogger("gene_order")
+logger = logging.getLogger("synmap.gene_order")
 logger.setLevel(logging.INFO)
 
 def main(input, output, gid1, gid2, feature1, feature2):
     ''' Returns a dag file
-    
+
     If feature1 or feature2 are genomic then the dag will be reordered and
-    generate a new file. Otherwise, the input dag file will be copied. 
+    generate a new file. Otherwise, the input dag file will be copied.
     '''
     genomic = False
 
@@ -25,9 +25,6 @@ def main(input, output, gid1, gid2, feature1, feature2):
     if not(feature1 == "genomic") and not(feature2 == "genomic"):
         logging.info("No genomic features were specified.")
     else:
-        #FIXME: Is this for a future addition?
-        # This was brought over from the perl script.
-        # Genomic names need to be processed seperately 
         genomic = True
 
     logging.info("Opening %s for converting to gene order.", input)
@@ -48,40 +45,57 @@ def main(input, output, gid1, gid2, feature1, feature2):
 
     if genomic:
         genomic_order = order_genes(input_file, feature1, feature2)
-    
+
     logging.info("Writing %s in gene order.", args.output)
 
     with open(output, "w") as fp:
         for line in input_file:
             if line.startswith("#"):
-                fp.write("%s\n" % line)
+                fp.write(line)
                 continue
-            
+
             fields = line.rstrip("\n\r").split("\t")
 
             if feature1 == "genomic":
                 fields[2] = genomic_order[1][fields[0]][fields[1]]['order']
                 fields[3] = genomic_order[1][fields[0]][fields[1]]['order']
             else:
-                item = re.split("\|\|", fields[1])[7]
-                fields[2] = item
-                fields[3] = item
+                item = re.split("\|\|", fields[1])
+
+                if not isinstance(item, list) or len(item) < 8:
+                    msg = ("The line could not be split for feature (%s)"
+                    + " in (%s).\n%s")
+                    logger.critical(msg, feature1, gid1, line)
+                    sys.exit(1)
+
+                fields[2] = item[7]
+                fields[3] = item[7]
 
             if feature2 == "genomic":
                 fields[6] = genomic_order[2][fields[4]][fields[5]]['order']
                 fields[7] = genomic_order[2][fields[4]][fields[5]]['order']
             else:
                 item = re.split("\|\|", fields[5])[7]
-                fields[6] = item
-                fields[7] = item
-           
+
+                if not isinstance(item, list) or len(item) < 8:
+                    msg = ("The line could not be split for feature (%s)"
+                    + " in (%s).\n%s")
+                    logger.critical(msg, feature2, gid2, line)
+                    sys.exit(1)
+
+                fields[6] = item[7]
+                fields[7] = item[7]
+
             output = reduce(lambda x, y: "\t".join([str(x),str(y)]), fields)
             fp.write("%s\n" % output)
-    return 0 
+    return 0
 
+# TODO: Some of the work should be able to be done in parallel
+# @by Evan Briones
+# @on 3/01/2013
 def order_genes(input_file, feature1, feature2):
     '''Returns dictionary containing the genomic order
-   
+
     The order of the genes in a genome are changed if the feature is genomic.
     The ordering of the genes is done by using the start position of its
     sequence.
@@ -96,6 +110,12 @@ def order_genes(input_file, feature1, feature2):
         field = line.rstrip("\n\r").split("\t")
         # FIXME: What was this intended to do? It's from the perl script
         #items = map(lambda x: re.split('\|\|', x), [fields[1], fields[5]])
+
+        if not isinstance(field, list) or len(field) < 7:
+            logger.critical("The line could not be split\n%s.", line)
+            sys.exit(1)
+
+        logger.debug("There are %d fields", len(field))
 
         if feature1 == "genomic":
             if not field[0] in genomic_order[1]:
@@ -115,16 +135,16 @@ def order_genes(input_file, feature1, feature2):
     if feature1 == "genomic":
         for chromosome in genomic_order[1].iterkeys():
             genes = genomic_order[1][chromosome].values();
-            sorted(genes, key=cmp_by_start_seq)
-            
+            genes = sorted(genes, key=cmp_by_start_seq)
+
             for (order, gene) in enumerate(genes, start=1):
                 gene['order'] = order
 
     if feature2 == "genomic":
         for chromosome in genomic_order[2].iterkeys():
             genes = genomic_order[2][chromosome].values();
-            sorted(items, key=cmp_by_start_seq)
-            
+            genes = sorted(genes, key=cmp_by_start_seq)
+
             for (order, gene) in enumerate(genes, start=1):
                 gene['order'] = order
 
@@ -148,9 +168,20 @@ if __name__ == "__main__":
                         help="The feature of the first genome")
     parser.add_argument("feature2",
                         help="The feature of the second genome")
+
+    parser.add_argument("--loglevel",
+                        help="This sets the log level")
     args = parser.parse_args()
+
+    if args.loglevel:
+        log_level = getattr(logging, args.loglevel.upper(), None)
+
+        if not isinstance(log_level, int):
+            raise ValueError('Invalid log level: %s' % log_level)
+        logger.setLevel(log_level)
 
     ret = main(args.input, args.output, args.gid1, args.gid2, args.feature1,
          args.feature2)
+
 
     sys.exit(ret)
